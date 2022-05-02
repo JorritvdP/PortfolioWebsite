59a645a9847b6dae647f8a19f8143a3f9cb6dbdc
JorritvdP <jvdplaats@hotmail.com>
2022-05-02 16:31:58 +0200

    intro part 2

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // JS part of website
+// test with dropdown menu
+/*function myFunction() {
+    document.getElementById("dropdown").classList.toggle("show");
+}
+
+window.onclick = (event) => {
+    if(!event.target.matches("navbar")) {
+        var dropdowns = document.getElementById("dropdown");
+        for (let i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if(openDropdown.classList.contains("show")) {
+                openDropdown.classlist.remove("show");
+            }
+        }
+    }
+};*/
