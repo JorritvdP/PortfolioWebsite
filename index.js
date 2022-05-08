2a6976a303aa984590eefe1d22ae81d44df15c5f
JorritvdP <jvdplaats@hotmail.com>
2022-05-08 15:55:42 +0200

    correctly updated slider

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // JS part of website
+//Slider for photos
+let slideIndex = 1;
+
+function displaySlides(n) {
+    var slides = document.getElementsByClassName("showSlide");
+    if(n > slides.length) {
+        slideIndex = 1;
+    }
+    if(n < 1) {
+        slideIndex=slides.length;
+    }
+
+    for(let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"
+    }
+    slides[slideIndex - 1].style.display = "block"
+}
+
+function nextSlide(n) {
+    displaySlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    displaySlides(slideIndex = n);
+}
+
+displaySlides(slideIndex);
